f59938fe1d471ce3de1b4a569abd7dce62a43656
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-11-29 23:39:51 +1100

    update repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,3 +1,5 @@
+use crate::parser::ast::Program;
+use crate::parser::{Parser, ParsingError};
 use std::io::{self, Write};
 
 pub struct Repl;
@@ -10,29 +12,22 @@ impl Repl {
         let mut writer = io::stdout();
 
         loop {
-            // writer.write_all(PROMPT.as_bytes())?;
-            // writer.flush()?;
-            //
-            // let mut buffer = String::new();
-            // let bytes_read = reader.read_line(&mut buffer)?;
-            //
-            // if bytes_read == 0 {
-            //     writeln!(writer)?;
-            //     break;
-            // }
-            //
-            // let mut errors = vec![];
-            // let nodes: Vec<_> = buffer
-            //     .as_str()
-            //     .ast_nodes()
-            //     .filter_map(|node| node.map_err(|e| errors.push(e)).ok())
-            //     .collect();
-            //
-            // if errors.len() > 0 {
-            //     errors.iter().for_each(|e| println!("{e:?}"));
-            // } else {
-            //     nodes.iter().for_each(|node| println!("{node:?}"));
-            // }
+            writer.write_all(PROMPT.as_bytes())?;
+            writer.flush()?;
+
+            let mut buffer = String::new();
+            let bytes_read = reader.read_line(&mut buffer)?;
+
+            if bytes_read == 0 {
+                writeln!(writer)?;
+                break;
+            }
+
+            let result = Parser::parse_program(buffer.as_str());
+            match result {
+                Ok(nodes) => println!("{nodes:?}"),
+                Err(errors) => errors.iter().for_each(|e| println!("{e:?}")),
+            }
         }
 
         Ok(())
