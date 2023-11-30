c7f19ce682c50dfc0975fbe64bf65e0a541311aa
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-11-30 11:34:30 +1100

    add evaluation for prefix operators

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,5 +1,5 @@
 use crate::evaluator::object::Object;
-use crate::parser::ast::{Expression, Program, Statement};
+use crate::parser::ast::{Expression, Prefix, Program, Statement};
 
 mod object;
 mod tests;
@@ -9,7 +9,7 @@ pub fn eval(program: Program) -> Result<Object, EvalError> {
     eval_statements(statements)
 }
 
-pub fn eval_statements(statements: Vec<Statement>) -> Result<Object, EvalError> {
+fn eval_statements(statements: Vec<Statement>) -> Result<Object, EvalError> {
     let mut result = Object::Null;
 
     for statement in statements.iter() {
@@ -24,13 +24,13 @@ pub fn eval_statements(statements: Vec<Statement>) -> Result<Object, EvalError> 
     Ok(result)
 }
 
-pub fn eval_expression(expression: &Expression) -> Result<Object, EvalError> {
+fn eval_expression(expression: &Expression) -> Result<Object, EvalError> {
     let result = match expression {
         Expression::Identifier(_) => todo!(),
         Expression::Integer(int) => Object::Integer(*int),
-        Expression::Prefix(_, _) => todo!(),
+        Expression::Prefix(operator, operand) => eval_prefix_expressions(operator, operand)?,
         Expression::Infix(_, _, _) => todo!(),
-        Expression::Boolean(_) => todo!(),
+        Expression::Boolean(val) => Object::Boolean(*val),
         Expression::If(_, _, _) => todo!(),
         Expression::Function(_, _) => todo!(),
         Expression::Call(_, _) => todo!(),
@@ -39,5 +39,30 @@ pub fn eval_expression(expression: &Expression) -> Result<Object, EvalError> {
     Ok(result)
 }
 
+fn eval_prefix_expressions(operator: &Prefix, operand: &Expression) -> Result<Object, EvalError> {
+    let right = eval_expression(operand)?;
+    match operator {
+        Prefix::Minus => eval_minus_operator_expression(&right),
+        Prefix::Bang => Ok(eval_bang_operator_expression(&right)),
+    }
+}
+
+fn eval_minus_operator_expression(object: &Object) -> Result<Object, EvalError> {
+    match object {
+        Object::Integer(int) => Ok(Object::Integer(-int)),
+        _ => return Err(EvalError::ExpectedInteger),
+    }
+}
+
+fn eval_bang_operator_expression(object: &Object) -> Object {
+    match object {
+        Object::Null => Object::Boolean(true),
+        Object::Integer(int) => Object::Boolean(if *int == 0 { true } else { false }),
+        Object::Boolean(val) => Object::Boolean(!val),
+    }
+}
+
 #[derive(Debug, PartialEq)]
-pub enum EvalError {}
+pub enum EvalError {
+    ExpectedInteger,
+}
--- src/evaluator/tests.rs
+++ src/evaluator/tests.rs
@@ -11,9 +11,97 @@ fn parse_and_eval(input: &str) -> Result<Object, EvalError> {
 }
 
 #[test]
-fn test_eval_work_for_integer_expression() {
+fn test_eval_for_integer_expression() {
     let input = "5";
     let expected = Object::Integer(5);
     let result = parse_and_eval(input).ok().unwrap();
     assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_for_boolean_expression() {
+    let input = "true";
+    let expected = Object::Boolean(true);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_one() {
+    let input = "!true";
+    let expected = Object::Boolean(false);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_two() {
+    let input = "!false";
+    let expected = Object::Boolean(true);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_three() {
+    let input = "!5";
+    let expected = Object::Boolean(false);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_four() {
+    let input = "!!true";
+    let expected = Object::Boolean(true);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_five() {
+    let input = "!!false";
+    let expected = Object::Boolean(false);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_bang_operator_six() {
+    let input = "!!5";
+    let expected = Object::Boolean(true);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_minus_operator_one() {
+    let input = "-5";
+    let expected = Object::Integer(-5);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_minus_operator_two() {
+    let input = "-10";
+    let expected = Object::Integer(-10);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_minus_operator_three() {
+    let input = "--10";
+    let expected = Object::Integer(10);
+    let result = parse_and_eval(input).ok().unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_eval_minus_operator_error_if_not_integer() {
+    let input = "-true";
+    let expected_error = EvalError::ExpectedInteger;
+    let error = parse_and_eval(input).err().unwrap();
+    assert_eq!(error, expected_error);
 }
